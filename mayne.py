fe2d66fb0bccbbaf75d1d4f0340d9a4f2f07ea93
Mark J. B <omnipotentmilk@gmail.com>
2023-11-30 22:23:32 -0500

    Translation V1
    Began wokring on the initial groundworks for the project, getting a basic transcription and translation wokring

---
+++ mayne.py
@@ -0,0 +1,27 @@
+def translation(mRNA_sequence):
+
+    codon = mRNA_sequence[:3]
+    mRNA_sequence = mRNA_sequence[3:]
+    return
+
+def codon_to_amino(codon):
+
+    # enter specific loops depending on first base in codon
+    if codon[0] == "U":
+    elif codon[0] == "C":
+    elif codon[0] == "A":
+    elif codon[0] == "G":
+
+
+
+
+
+
+
+
+
+
+    return
+
+codon_to_amino(["U","U","U"])
+translation(["U","U","U","U","U","U"])
